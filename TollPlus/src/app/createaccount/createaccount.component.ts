b214883fb2575592c5a4e85192728d26c08c7b7c
suseelkumar Annamraju <suseelkumara@gmail.com>
2017-08-07 14:06:16 +0530

    CSS changes for (#3)
    1) Main DIV changed color to #ffffff (white)

--- TollPlus/src/app/createaccount/createaccount.component.ts
+++ TollPlus/src/app/createaccount/createaccount.component.ts
@@ -52,8 +52,6 @@ export class CreateaccountComponent implements OnInit {
   idProofBusinesses=[];
   ngOnInit() {
 
-
-    debugger;
     let emailPattern:string = '/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/';
     this.user_Form=new FormGroup({
       first_Name:new FormControl("",Validators.compose([Validators.required,
