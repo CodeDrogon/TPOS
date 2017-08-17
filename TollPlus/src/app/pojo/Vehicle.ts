4313867a2007cdf583c853ee6e144b3f7c1b1853
Suseelkumar <suseelkumara@gmail.com>
2017-08-17 19:11:27 +0530

    Vehicle pojo changes

--- TollPlus/src/app/pojo/Vehicle.ts
+++ TollPlus/src/app/pojo/Vehicle.ts
@@ -1,3 +1,187 @@
 /**
  * Created by Suseel on 17-08-2017.
  */
+export class Vehicle{
+  private _plateNumber;
+  private _vehicleClass;
+  private _vehicle_Make
+  private _vehicle_Year;
+  private _vehicle_Model;
+  private _vehicle_Color;
+  private _registered_Country
+  private _registeredState;
+  private _startEffectiveDate;
+  private _startDateHours
+  private _startDateMins;
+  private _startDateSecs;
+  private _endEffectiveDate;
+  private _endDateHours;
+  private _endDateMins;
+  private _endDateSecs;
+  private _description;
+  private _isTemporaryLicencePlateNumber;
+  private _startEffectiveDateAndTime;
+  private _endEffectiveDateAndTime;
+
+
+  get plateNumber() {
+    return this._plateNumber;
+  }
+
+  set plateNumber(value) {
+    this._plateNumber = value;
+  }
+
+  get vehicleClass() {
+    return this._vehicleClass;
+  }
+
+  set vehicleClass(value) {
+    this._vehicleClass = value;
+  }
+
+  get vehicle_Make() {
+    return this._vehicle_Make;
+  }
+
+  set vehicle_Make(value) {
+    this._vehicle_Make = value;
+  }
+
+  get vehicle_Year() {
+    return this._vehicle_Year;
+  }
+
+  set vehicle_Year(value) {
+    this._vehicle_Year = value;
+  }
+
+  get vehicle_Model() {
+    return this._vehicle_Model;
+  }
+
+  set vehicle_Model(value) {
+    this._vehicle_Model = value;
+  }
+
+  get vehicle_Color() {
+    return this._vehicle_Color;
+  }
+
+  set vehicle_Color(value) {
+    this._vehicle_Color = value;
+  }
+
+  get registered_Country() {
+    return this._registered_Country;
+  }
+
+  set registered_Country(value) {
+    this._registered_Country = value;
+  }
+
+  get registeredState() {
+    return this._registeredState;
+  }
+
+  set registeredState(value) {
+    this._registeredState = value;
+  }
+
+  get startEffectiveDate() {
+    return this._startEffectiveDate;
+  }
+
+  set startEffectiveDate(value) {
+    this._startEffectiveDate = value;
+  }
+
+  get startDateHours() {
+    return this._startDateHours;
+  }
+
+  set startDateHours(value) {
+    this._startDateHours = value;
+  }
+
+  get startDateMins() {
+    return this._startDateMins;
+  }
+
+  set startDateMins(value) {
+    this._startDateMins = value;
+  }
+
+  get startDateSecs() {
+    return this._startDateSecs;
+  }
+
+  set startDateSecs(value) {
+    this._startDateSecs = value;
+  }
+
+  get endEffectiveDate() {
+    return this._endEffectiveDate;
+  }
+
+  set endEffectiveDate(value) {
+    this._endEffectiveDate = value;
+  }
+
+  get endDateHours() {
+    return this._endDateHours;
+  }
+
+  set endDateHours(value) {
+    this._endDateHours = value;
+  }
+
+  get endDateMins() {
+    return this._endDateMins;
+  }
+
+  set endDateMins(value) {
+    this._endDateMins = value;
+  }
+
+  get endDateSecs() {
+    return this._endDateSecs;
+  }
+
+  set endDateSecs(value) {
+    this._endDateSecs = value;
+  }
+
+  get description() {
+    return this._description;
+  }
+
+  set description(value) {
+    this._description = value;
+  }
+
+  get isTemporaryLicencePlateNumber() {
+    return this._isTemporaryLicencePlateNumber;
+  }
+
+  set isTemporaryLicencePlateNumber(value) {
+    this._isTemporaryLicencePlateNumber = value;
+  }
+
+
+  get startEffectiveDateAndTime() {
+    return this._startEffectiveDateAndTime;
+  }
+
+  set startEffectiveDateAndTime(value) {
+    this._startEffectiveDateAndTime = value;
+  }
+
+  get endEffectiveDateAndTime() {
+    return this._endEffectiveDateAndTime;
+  }
+
+  set endEffectiveDateAndTime(value) {
+    this._endEffectiveDateAndTime = value;
+  }
+}
